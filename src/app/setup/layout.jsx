8f43c4ef9c68f51186d0bf95836e4ef63530c8cb
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2026-04-14 04:15:23 +0200

    v1

---
+++ src/app/setup/layout.jsx
@@ -0,0 +1,9 @@
+// @/app/setup/layout.jsx (Setup Layout Server Component) 
+  
+export default async function SetupLayout({ children }) {  
+    return (
+         <>
+             {children}   
+        </>
+    );
+}
